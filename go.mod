2284718061755bb86b229fed13abaf6f5d2a2b82
João <jp.nmts@mgmail.com>
2024-12-28 23:53:26 -0300

    primeira aula

---
+++ go.mod
@@ -0,0 +1,3 @@
+module curso_golang_rocketseat
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var idade = 30
+
+func main() {
+	var x int8 = 10
+	nome := "Pedro"
+	sobrenome := "Pessoa"
+	fmt.Println(nome, sobrenome, idade)
+}
